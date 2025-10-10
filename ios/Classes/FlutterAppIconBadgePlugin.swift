9b6ac8bfc20ec96ae7220e969064ff190c96da05
jballard <jballard@centralert.com>
2025-10-10 15:08:15 -0400

    Update iOS project settings and dependencies, including migration to iOS 13.0, updating Flutter dependencies in pubspec.lock, and refactoring the FlutterAppIconBadgePlugin implementation. Removed obsolete Objective-C files and streamlined Swift plugin methods for badge management.

--- ios/Classes/FlutterAppIconBadgePlugin.swift
+++ ios/Classes/FlutterAppIconBadgePlugin.swift
@@ -10,10 +10,23 @@ public class FlutterAppIconBadgePlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+        case "updateBadge":
+          if let args = call.arguments as? Dictionary<String, Any>,
+            let count = args["count"] as? Int {
+            UIApplication.shared.applicationIconBadgeNumber = count
+            result(nil)
+          } else {
+            result(FlutterError.init(code: "bad args", message: nil, details: nil))
+          }
+        case "removeBadge":
+          UIApplication.shared.applicationIconBadgeNumber = 0
+          result(nil)
+        case "isAppBadgeSupported":
+          result(true)
+        case "isAppFocused":
+          result(UIApplication.shared.applicationState == .active)
+        default:
+          result(FlutterMethodNotImplemented)
     }
   }
 }
